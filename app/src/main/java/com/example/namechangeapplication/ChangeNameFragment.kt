6c98ec2c53c53278eafa7c1a5eeaf1809a91a446
debasihs <dev.dey161@gmail.com>
2024-07-10 23:00:34 +0530

    some aded

---
+++ app/src/main/java/com/example/namechangeapplication/ChangeNameFragment.kt
@@ -0,0 +1,30 @@
+package com.example.namechangeapplication
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChangeNameFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ChangeNameFragment()
+    }
+
+    private val viewModel: ChangeNameViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_change_name, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/namechangeapplication/ChangeNameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.namechangeapplication
+
+import androidx.lifecycle.ViewModel
+
+class ChangeNameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
