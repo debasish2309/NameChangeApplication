dd1e20ba87a6ae248cfd9640165e3cb9fe6d37d2
debasihs <dev.dey161@gmail.com>
2024-07-10 22:26:53 +0530

    new files added

---
+++ app/src/main/java/com/example/namechangeapplication/TestTwoFragment.kt
@@ -0,0 +1,30 @@
+package com.example.namechangeapplication
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TestTwoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TestTwoFragment()
+    }
+
+    private val viewModel: TestTwoViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_test_two, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/namechangeapplication/TestTwoViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.namechangeapplication
+
+import androidx.lifecycle.ViewModel
+
+class TestTwoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
